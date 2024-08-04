ff0eb71c681fb1fee50d9b247a50760877db1767
Dmitry Redkin <tanatonaut@gmail.com>
2024-08-04 20:06:38 +0300

    fixed collect

--- src/connectors/collect_connector.rs
+++ src/connectors/collect_connector.rs
@@ -1,13 +1,12 @@
 use std::sync::Arc;
 
 use actix_web::{web, HttpResponse, Responder};
-use eyre::Context;
 use serde::{Deserialize, Serialize};
 use ethers::{
-    core::types::TransactionRequest, middleware::SignerMiddleware, prelude::{abigen, Abigen}, providers::{Http, Middleware, Provider}, signers::{LocalWallet, Signer}, types::{Address, U256}
+    core::types::TransactionRequest, middleware::SignerMiddleware, prelude::abigen, providers::{Http, Middleware, Provider}, signers::{LocalWallet, Signer}, types::{Address, U256}
 };
 
-#[derive(Deserialize, Clone)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct CollectRequest {
     token: String,
     private_keys: Vec<String>,
@@ -22,89 +21,100 @@ struct CollectResponse {
 
 pub async fn make_collect(collect_req: web::Json<CollectRequest>) -> impl Responder {
     let collect_data = collect_req.into_inner();
-    if collect_data.token != "0x0000000000000000000000000000000000000000" {
-        abigen!(IERC20, "./token-abi.json");
 
-        let mut handles = vec![];
+    let mut handles = vec![];
 
-        for i in 0..collect_data.private_keys.len() {
-            let private_key = collect_data.private_keys[i].clone();
-            let provider;
+    for i in 0..collect_data.private_keys.len() {
+        let private_key = collect_data.private_keys[i].clone();
+        let provider;
 
-            match Provider::<Http>::try_from("https://sepolia.infura.io/v3/47bcfcab54cd4104a97fb13f84ae431e") {
-                Ok(provider_inner) => {provider = provider_inner}
-                Err(err) => {
-                    return HttpResponse::BadRequest().body(format!("Failed to get provider: {}", err));
-                }
-            };
-    
-            let wallet;
-            match LocalWallet::try_from(private_key) {
-                Ok(wallet_inner) => {wallet = wallet_inner}
-                Err(err) => {
-                    return HttpResponse::BadRequest().body(format!("Failed to create wallet: {}", err));
-                }
+        match Provider::<Http>::try_from("https://sepolia.infura.io/v3/47bcfcab54cd4104a97fb13f84ae431e") {
+            Ok(provider_inner) => {provider = provider_inner}
+            Err(err) => {
+                return HttpResponse::BadRequest().body(format!("Failed to get provider: {}", err));
             }
+        };
 
-            let client = SignerMiddleware::new(provider, wallet.with_chain_id(97u64));
-
-            let token_address;
-            match "0xc580f79b2C5aD06A32bCC0b331738a6D5A2Ac845".parse::<Address>() {
-                Ok(address_inn) => {token_address = address_inn}
-                Err(err) => {
-                    return HttpResponse::BadRequest().body(format!("Failed to parse address: {}", err));
-                }
+        let wallet;
+        match LocalWallet::try_from(private_key) {
+            Ok(wallet_inner) => {wallet = wallet_inner}
+            Err(err) => {
+                return HttpResponse::BadRequest().body(format!("Failed to create wallet: {}", err));
             }
+        }
 
-            let destination;
-            match collect_data.destination.parse::<Address>() {
-                Ok(destination_inner) => {destination = destination_inner}
-                Err(err) => {
-                    return HttpResponse::BadRequest().body(format!("Failed to parse amount: {}", err));
-                }
+        let client = SignerMiddleware::new(provider, wallet.with_chain_id(11155111u64));
+
+        let token_address;
+        match collect_data.token.as_str().parse::<Address>() {
+            Ok(address_inn) => {token_address = address_inn}
+            Err(err) => {
+                return HttpResponse::BadRequest().body(format!("Failed to parse address: {}", err));
             }
+        }
 
-            let amount;
-            match U256::from_dec_str(collect_data.amounts[i].as_str()) {
-                Ok(amount_inner) => {amount = amount_inner}
-                Err(err) => {
-                    return HttpResponse::BadRequest().body(format!("Failed to parse amount: {}", err));
-                }
+        let destination;
+        match collect_data.destination.parse::<Address>() {
+            Ok(destination_inner) => {destination = destination_inner}
+            Err(err) => {
+                return HttpResponse::BadRequest().body(format!("Failed to parse amount: {}", err));
             }
+        }
+
+        let amount;
+        match U256::from_dec_str(collect_data.amounts[i].as_str()) {
+            Ok(amount_inner) => {amount = amount_inner}
+            Err(err) => {
+                return HttpResponse::BadRequest().body(format!("Failed to parse amount: {}", err));
+            }
+        }
 
 
-            handles.push(tokio::spawn(async move {
+        handles.push(tokio::spawn(async move {
+            if !token_address.is_zero() {
+                abigen!(IERC20, "./token-abi.json");
                 let token_contract = IERC20::new(token_address, Arc::new(client));
 
                 if let Ok(tx) = token_contract.transfer(destination, amount).send().await {
-                    let receipt = tx.await.expect("tx dropped from mempool");
-                    return receipt.expect("Cant unwrap option").transaction_hash.to_string();
-                };
-
-                "".to_string()
-            }));
-        }
-
-        let mut txs = vec![];
-
-        for handle in handles {
-            match handle.await {
-                Ok(tx) => {txs.push(tx)}
-                Err(err) => {
-                    return HttpResponse::InternalServerError().body(format!("Failed to send transaction: {}", err));
+                    let tx_hash = hex::encode(tx.tx_hash().as_bytes());
+                    tx.await.expect("tx dropped from mempool");
+                    return tx_hash;
+            };
+    
+                return "".to_string();
+            } else {
+                if let Ok(tx) = client.send_transaction(
+                    TransactionRequest::new()
+                        .to(destination)
+                        .value(amount), 
+                    None).await {
+                        let tx_hash = hex::encode(tx.tx_hash().as_bytes());
+                        tx.await.expect("tx dropped from mempool");
+                        return tx_hash;
+                    };
+        
+                    return "".to_string();
                 }
-
-            }
-        }
-        
-        for tx in txs.clone() {
-            if tx == "" {
-                return HttpResponse::InternalServerError().body(format!("Failed to send transaction"));
-            }
-        }
-        
-        return HttpResponse::Ok().json(txs);
+        }));
     }
 
-    return HttpResponse::Ok().json({});
+    let mut txs = vec![];
+
+    for handle in handles {
+        match handle.await {
+            Ok(tx) => {txs.push(tx)}
+            Err(err) => {
+                return HttpResponse::InternalServerError().body(format!("Failed to send transaction: {}", err));
+            }
+
+        }
+    }
+    
+    for tx in txs.clone() {
+        if tx == "" {
+            return HttpResponse::InternalServerError().body(format!("Failed to send transaction"));
+        }
+    }
+    
+    return HttpResponse::Ok().json(txs);
 }
